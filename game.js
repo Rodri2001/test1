7f00e3e46c893d0fadbad9c1bb2b4bed0a6ac69e
Rodri2001 <rodrigodamianmartinez2001@gmail.com>
2023-04-12 23:00:50 -0300

    retomar en algun momento esto

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+let snakeHead;
+let snakeSpeed = 100;
+let currentDirection = 'right';
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
@@ -13,8 +17,52 @@ let config = {
   
   var game = new Phaser.Game(config);
   
-  function preload() {}
+  function preload() {
+    this.load.image('snake-head', 'assets/snake-head.png');
+    this.load.audio('move', 'assets/move.wav');
+  }
   
-  function create() {}
+  function create() {
+    snakeHead = this.add.sprite(320, 240, 'snake-head');
+    snakeHead.setScale(4);
+    snakeHead.setOrigin(0.5);
   
-  function update() {}
+    this.sound.add('move');
+  
+    this.input.keyboard.on('keydown', function (event) {
+      switch (event.keyCode) {
+        case 37:
+          currentDirection = 'left';
+          break;
+        case 38:
+          currentDirection = 'up';
+          break;
+        case 39:
+          currentDirection = 'right';
+          break;
+        case 40:
+          currentDirection = 'down';
+          break;
+      }
+    });
+  }
+  
+  
+  function update() {
+    switch (currentDirection) {
+        case 'left':
+          snakeHead.x -= snakeSpeed * (delta / 1000);
+          break;
+        case 'up':
+          snakeHead.y -= snakeSpeed * (delta / 1000);
+          break;
+        case 'right':
+          snakeHead.x += snakeSpeed * (delta / 1000);
+          break;
+        case 'down':
+          snakeHead.y += snakeSpeed * (delta / 1000);
+          break;
+      }
+    
+      this.sound.play('move');
+  }
